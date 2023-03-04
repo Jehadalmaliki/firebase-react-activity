f12619a2c779f96105a68b633ed6c9eb2a1e57cb
Jehad Almaliki <yourgitemail@example.com>
2023-03-04 21:17:47 +0300

    Initialize Firebase

--- src/util/firebase.js
+++ src/util/firebase.js
@@ -1,1 +1,18 @@
 // Your firebase initial code should come here.
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyB5xNxc7iWChiPIDwy60hcWSuKdkiHzEbE",
+  authDomain: "recoded-class-activite.firebaseapp.com",
+  projectId: "recoded-class-activite",
+  storageBucket: "recoded-class-activite.appspot.com",
+  messagingSenderId: "119439466455",
+  appId: "1:119439466455:web:5e2ba87272b5f909247592"
+};
+
+// Initialize Firebase
+ const app = initializeApp(firebaseConfig);
